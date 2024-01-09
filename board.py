18474abe57c8b55e802a0083de5b144491e3a1e5
Carlos Santiago Rojas Calderón <carlosantirc@gmail.com>
2024-01-09 09:46:13 -0500

    Player is able to make a move

--- board.py
+++ board.py
@@ -2,15 +2,75 @@ class Board:
     def __init__(self) -> None:
         self.bit_array = [None for _ in range(9)]
     
-    def show_tic_tac_toe(self) -> None:
-        for i in range(0, len(self.bit_array)):
-               if i % 3 == 2:
-                   print(self.bit_array[i] if self.bit_array[i] is not None else ' ')
-                   if i != len(self.bit_array) - 1:
-                       print('---------')
-               else:
-                   print(self.bit_array[i] if self.bit_array[i] is not None else ' ', end=' | ')
-
     
     def __str__(self) -> str:
+        """Print the current state of the bits
+
+        Returns:
+            str: The array of bits
+        """
         return f"{self.bit_array}"
+    
+    
+    def show_tic_tac_toe(self) -> None:
+        """Print the tic-tac-toe board to the console.
+        
+        None values are printed as spaces, 1 as X and 0 as O
+        """
+        print("\n")
+        for i in range(0, len(self.bit_array)):
+            if i % 3 == 2:
+                if self.bit_array[i] == 1:
+                    print('X')
+                elif self.bit_array[i] == 0:
+                    print('O')
+                else:
+                    print(' ')
+                    
+                if i != len(self.bit_array) - 1:
+                    print('---------')
+            else:
+                if self.bit_array[i] == 1:
+                    print('X', end=' | ')
+                elif self.bit_array[i] == 0:
+                    print('O', end=' | ')
+                else:
+                    print(' ', end=' | ')
+    
+    
+    def make_move_player(self, row: int, column: int) -> bool:
+        """The player make a moved based on the row and the column in the array
+
+        Args:
+            row (int): Number of the row
+            column (int): Number of the column
+
+        Returns:
+            bool: True if the move was made, False otherwise
+        """
+        
+        moved = False
+        # Subtract one to match the index of the array
+        row -= 1 
+        column -= 1
+        
+        # Make a move in the index of the array
+        if row == 0:
+            if self.bit_array[column] == None:
+                self.bit_array[column] = 1
+                moved = True
+            else:
+                print("\nERROR: The position it's already occupied select another")
+        elif row == 1:
+            if self.bit_array[column + row + 2] == None:
+                self.bit_array[column + row + 2] = 1
+                moved = True
+            else:
+                print("\nERROR: The position it's already occupied select another")
+        elif row == 2:
+            if self.bit_array[column + row + 4] == None:
+                self.bit_array[column + row + 4] = 1
+                moved = True
+            else:
+                print("\nERROR: The position it's already occupied select another")
+        return moved
--- main.py
+++ main.py
@@ -1,9 +1,40 @@
 from board import Board
 
+
 def main():
     board = Board()
-    print(str(board))
     board.show_tic_tac_toe()
+    get_move(board, False)
 
+def get_move(board: Board, finished: bool) -> None:
+    while not finished:
+        try:
+            correct_col = False
+            correct_row = False
+            print("\nYou play with 'X' make a move")
+
+            row = int(input("Type the number of the row (1 to 3): "))
+            column = int(input("Type the number of the column (1 to 3): "))
+
+            if 1 <= row <= 3:
+                correct_row = True
+            if 1 <= column <= 3:
+                correct_col = True
+
+            if correct_col and correct_row:
+                board.make_move_player(row, column)
+                board.show_tic_tac_toe()
+
+            if not correct_row and not correct_col:
+                print("ERROR: Type the number of the row and column in the specified range")
+            elif not correct_row:
+                print("ERROR: Type the number of the row in the specified range")
+            elif not correct_col:
+                print("ERROR: Type the number of the column in the specified range")
+
+        except ValueError:
+            print("ERROR: Type a number")
+
+        
 if __name__ == "__main__":
     main()
