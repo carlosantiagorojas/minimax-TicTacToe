dbe8420e7b009d3b8fb3421800e0c1679bd6af55
Carlos Santiago Rojas Calderón <carlosantirc@gmail.com>
2024-01-08 22:04:40 -0500

    Show board

--- board.py
+++ board.py
@@ -1,7 +1,16 @@
 class Board:
     def __init__(self) -> None:
-        self.bit_array = [0 for _ in range(9)]
+        self.bit_array = [None for _ in range(9)]
+    
+    def show_tic_tac_toe(self) -> None:
+        for i in range(0, len(self.bit_array)):
+               if i % 3 == 2:
+                   print(self.bit_array[i] if self.bit_array[i] is not None else ' ')
+                   if i != len(self.bit_array) - 1:
+                       print('---------')
+               else:
+                   print(self.bit_array[i] if self.bit_array[i] is not None else ' ', end=' | ')
+
     
     def __str__(self) -> str:
         return f"{self.bit_array}"
-        
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from board import Board
 def main():
     board = Board()
     print(str(board))
+    board.show_tic_tac_toe()
 
 if __name__ == "__main__":
     main()
